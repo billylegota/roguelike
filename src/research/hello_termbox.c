945c8348ecfc35cccab677a18065490687ed6439
William Blount <william.n.blount@gmail.com>
2018-07-25 03:24:22 +0000

    Fixed Makefile and added sleep to hello_termbox.

--- src/research/hello_termbox.c
+++ src/research/hello_termbox.c
@@ -35,6 +35,9 @@ int main(void) {
     // Show the changes.
     tb_present();
 
+    // Sleep for a bit.
+    sleep(5);
+
     // Close the screen and exit.
     tb_shutdown();
     return 0;
@@ -44,7 +47,7 @@ int main(void) {
  * @brief Writes a message with rainbow fg (black bg) at the given position.
  */
 void write_rainbow_fg(int x, int y, char *msg) {
-    for(int i = 0; msg[i] != NULL; i++) {
+    for(int i = 0; msg[i] != '\0'; i++) {
         uint32_t ch = (uint32_t) msg[i];
         uint16_t fg = RAINBOW[i % 6];
         uint16_t bg = TB_BLACK;
@@ -57,7 +60,7 @@ void write_rainbow_fg(int x, int y, char *msg) {
  * @brief Writes a message with rainbow bg (black fg) at the given position.
  */
 void write_rainbow_bg(int x, int y, char *msg) {
-    for(int i = 0; msg[i] != NULL; i++) {
+    for(int i = 0; msg[i] != '\0'; i++) {
         uint32_t ch = (uint32_t) msg[i];
         uint16_t fg = TB_BLACK;
         uint16_t bg = RAINBOW[i % 6];
@@ -70,7 +73,7 @@ void write_rainbow_bg(int x, int y, char *msg) {
  * @brief Writes a message with rainbow fg and bg at the given position.
  */
 void write_rainbow_fg_bg(int x, int y, char *msg) {
-    for(int i = 0; msg[i] != NULL; i++) {
+    for(int i = 0; msg[i] != '\0'; i++) {
         uint32_t ch = (uint32_t) msg[i];
         uint16_t fg = RAINBOW[i % 6];
         uint16_t bg = RAINBOW[5 - i % 6];
